0a7278af8ee079106c15db031453befd852bb9a5
Shall16 <shall130@rsdmo.org>
2018-06-18 19:42:43 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -1,9 +1,9 @@
 
 # Challenge 1: Fill this string in with your favorite word
 
-your_favorite_word = ""
+your_favorite_word = "adagio"
 
-puts "My favorite word is #{J'aime le fromage}"
+puts "My favorite word is #{adagio}"
 
 # Challenge 2: Capitalize every letter in your_favorite_word using a built in Ruby method
 puts .capitalize
